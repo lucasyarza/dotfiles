9dd38487ba856dc94c4b1617b75e5dc7137b5d85
lucasyarza <ldeyarza@mercadona.es>
2022-04-10 15:05:37 +0200

    Finde kubeconfig

--- shell/_functions/kubernetes.sh
+++ shell/_functions/kubernetes.sh
@@ -5,13 +5,13 @@
 kubectl() {
   unset -f kubectl
   source $ZSH/plugins/kubectl/kubectl.plugin.zsh
-  source <(kubectl completion zsh)
+  source < (kubectl completion zsh)
   kubectl "$@"
 }
 
 _ctxkubeconfig_complete() {
       if [ -z "$_KUBECONFG_COMPLETE_PATH" ]; then
-          _KUBECONFG_COMPLETE_PATH="$(ls ~/.kube)"
+          _KUBECONFG_COMPLETE_PATH="$(ls ~/.kube | grep kubeconfig | fzf --height=40% --layout=reverse --info=inline --border --margin=1 --padding=1)"
       fi
       local opts="$_KUBECONFG_COMPLETE_PATH"
       local cur=${COMP_WORDS[COMP_CWORD]}
