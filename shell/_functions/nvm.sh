18d7355dcae5f295ed2340c866c682668798780b
lucasyarza <ldeyarza@mercadona.es>
2022-04-10 15:04:27 +0200

    Suport lazy load nvim

--- shell/_functions/nvm.sh
+++ shell/_functions/nvm.sh
@@ -22,3 +22,10 @@ npm() {
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
     npm "$@"
 }
+nvim() {
+    unset -f nvm node npm nvim
+    export NVM_DIR=~/.nvm
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    nvim "$@"
+}
