ec97f1d2d5adc86a1b3340babd876fd7ffc90e01
lucasyarza <ldeyarza@mercadona.es>
2022-12-04 07:15:09 +0100

    Lazyload npm and node with lunarvim

--- shell/_functions/nvm.sh
+++ shell/_functions/nvm.sh
@@ -29,3 +29,10 @@ nvim() {
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
     nvim "$@"
 }
+lvim() {
+    unset -f nvm node npm lvim
+    export NVM_DIR=~/.nvm
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    lvim "$@"
+}
